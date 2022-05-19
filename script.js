d62c9621fadd162530faf9828cfc34a9f2919418
patocodestoo <patcodes@protonmail.com>
2022-05-18 20:29:54 -0400

    files

---
+++ script.js
@@ -0,0 +1,16 @@
+<div class="navbar-container">
+
+<nav>
+  <div class="navbar-logo">
+    <label id="MainLogoText"><a href="#">Patricio Contreras</a></label>
+</div>
+    <ul >
+       
+<li> <a href="" id="menu-button"></a></li> 
+<li id="NavBarText"> <a href="#">Work</a></li>
+<li id="NavBarText"><a href="#">About</a> </li>
+<li id="NavBarText"><a href="#"> Contact</a> </li>
+
+</ul>
+</nav>
+</div>
